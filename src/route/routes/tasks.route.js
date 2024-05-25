18d3f42074bc5cfc13137665c8b4ea5af5a53208
adhil <adhil.mhdk28@gmail.com>
2024-05-25 18:36:01 +0530

    fix : corrected edpoints

--- src/route/routes/tasks.route.js
+++ src/route/routes/tasks.route.js
@@ -4,8 +4,8 @@ import RequestHandler from "../../utils/RequestHandler.js";
 
 export const TasksRouter = Router();
 
-TasksRouter.get('/get', (req, res) => RequestHandler(req, res, tasksService.getTasksService));
-TasksRouter.get('/get/:id', (req, res) => RequestHandler(req, res, tasksService.getTaskByIdService));
-TasksRouter.post('/create', (req, res) => RequestHandler(req, res, tasksService.createTaskService));
-TasksRouter.put('/update/:id', (req, res) => RequestHandler(req, res, tasksService.updateTaskService));
-TasksRouter.delete('/delete/:id', (req, res) => RequestHandler(req, res, tasksService.deleteTaskService));
+TasksRouter.get('/', (req, res) => RequestHandler(req, res, tasksService.getTasksService));
+TasksRouter.get('/:id', (req, res) => RequestHandler(req, res, tasksService.getTaskByIdService));
+TasksRouter.post('/', (req, res) => RequestHandler(req, res, tasksService.createTaskService));
+TasksRouter.put('/:id', (req, res) => RequestHandler(req, res, tasksService.updateTaskService));
+TasksRouter.delete('/:id', (req, res) => RequestHandler(req, res, tasksService.deleteTaskService));
